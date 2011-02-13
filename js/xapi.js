ad435acde2f2f286b9218deac28379f828c8fad6
vsandre <vsandre@web.de>
2011-02-13 16:14:53 +0100

    some checks for bbox (maximum, is number, lower coord)

--- js/xapi.js
+++ js/xapi.js
@@ -14,22 +14,80 @@ $(document).ready(function() {
   var goog =  new OpenLayers.Projection("EPSG:900913");
   var latlon = new OpenLayers.Projection("EPSG:4326");
 
-  var outofboundscheck = function(coord,lat_or_lon) {
-    var bounds_min = 0;
-    var bounds_max = 0;
-    if (lat_or_lon == "lat") {
-      bounds_min = -90;
-      bounds_max = 90;
-    }
-    if (lat_or_lon == "lon") {
-      bounds_min = -180;
-      bounds_max = 180;
-    }
-    if ((bounds_min==0)&&(bounds_max==0)) { return null; }
-    if (coord < bounds_min) { coord = bounds_min; }
-    if (coord > bounds_max) { coord = bounds_max; }
-    return coord;
+  // bbox class
+  bbox_coord = function(ntype,nvalue) {
+    var value; // start coord
+    var type;  // lat or lon
+    this.settype = function(newtype) {
+      if((newtype=='lat')||(newtype=='lon')) {
+        this.type = newtype;
+        return 1;
+      } else {
+        return 0;
+      }
+    };
+    this.gettype = function() {
+      return this.type;
+    };
+    this.set = function(newvalue) {
+      newvalue = parseFloat(newvalue);
+      if(isNaN(newvalue)) {
+        return 0;
+      } else {
+        newvalue = this.outofboundscheck(newvalue);
+        this.value = newvalue.toFixed(5);
+        return 1;
+      }
+    };
+    this.get = function() {
+      return this.value;
+    };
+    this.outofboundscheck = function(coord) {
+      var bounds_min = 0;
+      var bounds_max = 0;
+      if (this.type == "lat") {
+        bounds_min = -90;
+        bounds_max = 90;
+      }
+      if (this.type == "lon") {
+        bounds_min = -180;
+        bounds_max = 180;
+      }
+      if ((bounds_min==0)&&(bounds_max==0)) { return null; }
+      if (coord < bounds_min) { coord = bounds_min; }
+      if (coord > bounds_max) { coord = bounds_max; }
+      return coord;
+    };
+    // set initial values
+    this.settype(ntype || 'lat');
+    this.set(nvalue || 0);
   };
+
+  bbox_set = function(cleft,cbottom,cright,ctop) {
+    this.left = new bbox_coord('lon',cleft || -180);
+    this.bottom = new bbox_coord('lat',cbottom || -90);
+    this.right = new bbox_coord('lon',cright || 180);
+    this.top = new bbox_coord('lat',ctop || 90);
+    this.check = function() {
+      var updateNeeded = false;
+      var temp;
+      if(parseFloat(this.left.get()) > parseFloat(this.right.get())) {
+        temp = this.left.get();
+        this.left.set(this.right.get());
+        this.right.set(temp);
+        updateNeeded = true;
+      };
+      if(parseFloat(this.bottom.get()) > parseFloat(this.top.get())) {
+        temp = this.bottom.get();
+        this.bottom.set(this.top.get());
+        this.top.set(temp);
+        updateNeeded = true;
+      };
+      return updateNeeded;
+    };
+  };
+
+  var bbox = new bbox_set(-180,-90,180,90);
 
   var bboxVectors = new OpenLayers.Layer.Vector("Bounding Box", {});
   map.addLayer(bboxVectors);
@@ -55,10 +113,11 @@ $(document).ready(function() {
         var ur = map.getLonLatFromPixel(new OpenLayers.Pixel(bounds.right, bounds.top)); 
         var llLat = ll.transform(map.getProjectionObject(), latlon);
         var urLat = ur.transform(map.getProjectionObject(), latlon);
-        $('#bbox_left').val(outofboundscheck(llLat.lon.toFixed(5),'lon'));
-        $('#bbox_bottom').val(outofboundscheck(llLat.lat.toFixed(5),'lat'));
-        $('#bbox_right').val(outofboundscheck(urLat.lon.toFixed(5),'lon'));
-        $('#bbox_top').val(outofboundscheck(urLat.lat.toFixed(5),'lat'));
+        bbox.left.set(llLat.lon);
+        bbox.bottom.set(llLat.lat);
+        bbox.right.set(urLat.lon);
+        bbox.top.set(urLat.lat);
+        update_inputfields();
         update_bbox();
         update_results();
       }
@@ -88,18 +147,29 @@ $(document).ready(function() {
   };
 
   // Function to return a bbox string
-  var bbox = function() {
-    var b = 'bbox=' + $('#bbox_left').val() + ',' + $('#bbox_bottom').val() +
-      ',' + $('#bbox_right').val() + ',' + $('#bbox_top').val();
+  var bboxstring = function() {
+    var b = 'bbox=' + bbox.left.get() + ',' + bbox.bottom.get() +
+      ',' + bbox.right.get() + ',' + bbox.top.get();
     return b;
+  }
+
+  var update_inputfields = function() {
+    $('#bbox_left').val(bbox.left.get());
+    $('#bbox_bottom').val(bbox.bottom.get());
+    $('#bbox_right').val(bbox.right.get());
+    $('#bbox_top').val(bbox.top.get());
+  }
+  
+  var read_inputfields = function() {
+    bbox.left.set($('#bbox_left').val());
+    bbox.bottom.set($('#bbox_bottom').val());
+    bbox.right.set($('#bbox_right').val());
+    bbox.top.set($('#bbox_top').val());
   }
 
   // Update the bbox from the text to the map
   var update_bbox = function() {
-    var bounds = new OpenLayers.Bounds( parseFloat($('#bbox_left').val()),
-                                        parseFloat($('#bbox_bottom').val()),
-                                        parseFloat($('#bbox_right').val()),
-                                        parseFloat($('#bbox_top').val()));
+    var bounds = new OpenLayers.Bounds( bbox.left.get(), bbox.bottom.get(), bbox.right.get(), bbox.top.get());
     bounds.transform(latlon, goog);
     
     var bboxGeom = bounds.toGeometry();
@@ -122,11 +192,11 @@ $(document).ready(function() {
     if ($('#searchbytag').is(':checked')) {
       results = results + tagsearch();
       if ($('#searchbybbox').is(':checked')) {
-        results = results + '[' + bbox() + ']'; };
+        results = results + '[' + bboxstring() + ']'; };
     }
     else {
       if ($('#searchbybbox').is(':checked')) {
-        results = results + 'map?' + bbox(); }
+        results = results + 'map?' + bboxstring(); }
     };
     $('#results').text(results);
     $('#results').attr('href', results);
@@ -174,15 +244,27 @@ $(document).ready(function() {
   });
 
   $('#bbox_top').change(function() {
+    read_inputfields();
+    if (bbox.check()) { $('#bbox_bottom').focus(); };
+    update_inputfields();
     update_bbox();
     update_results();});
   $('#bbox_bottom').change(function() {
+    read_inputfields();
+    if (bbox.check()) { $('#bbox_top').focus(); };
+    update_inputfields();
     update_bbox();
     update_results();});
   $('#bbox_left').change(function() {
+    read_inputfields();
+    if (bbox.check()) { $('#bbox_right').focus(); };
+    update_inputfields();
     update_bbox();
     update_results();});
   $('#bbox_right').change(function() {
+    read_inputfields();
+    if (bbox.check()) { $('#bbox_left').focus(); };
+    update_inputfields();
     update_bbox();
     update_results();});
 
